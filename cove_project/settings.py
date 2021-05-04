b8563c1e7c3a70f070795174d6557fe5eaa47abd
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-05-04 18:52:30 -0400

    settings: Remove server_max_value_length

--- cove_project/settings.py
+++ cove_project/settings.py
@@ -174,8 +174,5 @@ if not DEBUG:
         'default': {
             'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
             'LOCATION': '127.0.0.1:11211',
-            'OPTIONS': {
-                'server_max_value_length': 1024 * 1024 * 2,
-            }
         }
     }
