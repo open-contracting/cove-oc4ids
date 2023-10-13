933015e99d6f2f89fd4640c4a7ee7e8ab29c06d9
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-10-13 19:42:01 -0400

    chore: Use getenv() instead of environ.get()

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
-BROWSER = os.environ.get("BROWSER", "ChromeHeadless")
+BROWSER = os.getenv("BROWSER", "ChromeHeadless")
 
 
 @pytest.fixture(scope="module")
