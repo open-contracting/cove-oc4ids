d030384a4b14b53645a4d712cdc13ca8e44f2902
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-11-20 14:48:17 -0500

    build: Upgrade flattentool. Remove old comment.

--- cove_oc4ids/views.py
+++ cove_oc4ids/views.py
@@ -61,8 +61,7 @@ def explore_oc4ids(request, pk):
 
         schema_oc4ids = SchemaOC4IDS(lib_cove_oc4ids_config=lib_cove_oc4ids_config)
 
-        # Flatten Tool has catastrophically bad performance on even a 50 MB file (uses 3 GB). In the last 14 days as of
-        # 2020-10-06, `flattened.xlsx` has been requested only once. As such, this feature is disabled.
+        # This feature is disabled, because `flattened.xlsx` was requested once in 14 days as of 2020-10-06.
         # context.update(convert_json(upload_dir, upload_url, file_name, lib_cove_oc4ids_config,
         #                             schema_url=schema_oc4ids.schema_url, replace=True,
         #                             request=request, flatten=True))
