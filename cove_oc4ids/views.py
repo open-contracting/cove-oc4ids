909820f7c6c775f0d336290d77ff93224e598dff
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-09-03 22:34:47 -0400

    fix: Use gettext_lazy only if called outside individual request

--- cove_oc4ids/views.py
+++ cove_oc4ids/views.py
@@ -6,7 +6,7 @@ from cove.views import cove_web_input_error, explore_data_context
 from django.conf import settings
 from django.shortcuts import render
 from django.utils.html import format_html
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext as _
 from libcove.lib.converters import convert_spreadsheet
 from libcoveoc4ids.common_checks import common_checks_oc4ids
 from libcoveoc4ids.config import LibCoveOC4IDSConfig
