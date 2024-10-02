01a93bf056e4a550146630221bdb17bd45c74f96
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-10-02 14:39:05 -0400

    feat: Increase DATA_UPLOAD_MAX_MEMORY_SIZE to 5MB (2.5MB default)

--- core/settings.py
+++ core/settings.py
@@ -218,6 +218,9 @@ LANGUAGES = (
 MEDIA_ROOT = os.getenv("MEDIA_ROOT", "/data/media/" if production else BASE_DIR / "media/")
 MEDIA_URL = "media/"
 
+# https://docs.djangoproject.com/en/4.2/ref/settings/#data-upload-max-memory-size
+DATA_UPLOAD_MAX_MEMORY_SIZE = 52428800  # 5 MB
+
 if production:
     CACHES = {
         "default": {
